33f432194091f2928257363164c62fdb531447f1
王 晔 <1445676266@qq.com>
2022-11-29 17:09:25 +0800

    initial

---
+++ apps/helloworld.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/features2d.hpp>
+// #include <opencv2/xfeatures2d.hpp>
+// #include <opencv2/highgui.hpp>
+// #include <opencv2/imgproc.hpp>
+#include <vector>
+
+using namespace std;
+using namespace cv;
+int main()
+{
+    std::cout << "hello" << std::endl;
+    cv::Ptr<cv::SIFT> siftDetector = cv::SIFT::create(500);
+	// Ptr<SIFT> detector = SIFT::create(500);
+    // cv::Ptr<cv::feature
+    // cv::Ptr<cv::Feature2D> sift = cv::Feature2D:: SIFT::create();
+    return 0;
+}
